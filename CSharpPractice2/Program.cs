85a1e9c62d42e46d81792f467427da8288baf346
Jeffrey North <JNorth@tql.com>
2021-05-31 12:55:21 -0400

    MO PRACTICE

--- CSharpPractice2/Program.cs
+++ CSharpPractice2/Program.cs
@@ -6,6 +6,35 @@ namespace CSharpPractice2
     {
         static void Main(string[] args)
         {
+            /*
+            {
+                int[] nbrs =
+            {
+                   754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+                   187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+                   460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+                   690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+                   435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+                };
+                int[] nbrs =
+                {
+                   754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+                   187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+                   460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+                   690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+                   435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+                };
+                var highest = 0;
+                var lowest = 1000;
+                foreach(var nbr in nbrs)
+                {
+                    if (nbr > highest)
+                        highest = nbr;
+                    if (nbr < lowest)
+                        lowest = nbr;
+                }
+            */
+            /*
             int[] nbrs =
             {
                754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
@@ -14,19 +43,26 @@ namespace CSharpPractice2
                690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
                435, 643, 809, 874, 906, 620, 328, 369, 426, 561
             };
-            var highest = 0;
-            var lowest = 1000;
-            foreach(var nbr in nbrs)
+              
+                var total = 0;
+                var count = 0;
+                foreach(var nbr in nbrs)
+                {
+                    total += nbr;
+                    count++;
+                }
+            var avg = total / count;
+            Console.WriteLine($"Average is {avg} ");
+            */
+
+            var nbr = 1;
+            while( nbr >= 25)
             {
-                if (nbr > highest)
-                    highest = nbr;
-                if (nbr < lowest)
-                    lowest = nbr;
+                var cubed = nbr * nbr * nbr;
+                Console.WriteLine($"{cubed} ");
+                nbr += 2;
+
             }
-
-
-            Console.WriteLine($"Highest: {highest}; Lowest: { lowest} ");
-                    
         }
     }
 }
